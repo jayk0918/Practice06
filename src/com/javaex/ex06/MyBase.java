aa26637fcc7cb7ba732d17533d966056b3f6346c
Mocking_jayk0918 <jesper2able@gmail.com>
2022-04-27 22:05:31 +0900

    1.0(java_practice ex06Edit 22.04.27 22:05)

--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -4,18 +4,27 @@ public class MyBase extends Base{
     //코드작성
 	
 	public void service(String state) {
-		if (state.equals("낮")) {
-			System.out.println("낮에는 열심히 수업듣자");
-		}else if(state.equals("오후")){
-			System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+		if(state.equals("낮")) {
+			day();
+		}else if(state.equals("밤")) {
+			night();
 		}else {
-			System.out.println("밤에는 숙면");
+			noon();
 		}
-		
 	}
 	
 	
+	public void day() {
+		System.out.println("낮에는 열심히 수업듣자");
+	}
 	
+	public void noon() {
+		System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+	}
+	
+	public void night() {
+		System.out.println("밤에는 숙면");
+	}
 	
     
 }
